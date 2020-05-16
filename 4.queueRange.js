26c08f7de4bba982f5cd98a8c56049f165bf4aa4
darsuabasi <uduakabasiabasiurua@pursuit.org>
2020-05-16 17:40:51 -0400

    psuedocode 4 queue range

--- 4.queueRange.js
+++ 4.queueRange.js
@@ -35,7 +35,12 @@
 
 // Write your function here
 const range = (queue) => {
-
+    if(queue.isEmpty()) return [];
+    let range = maxOfArr - minOfArr
+    while(!queue.isEmpty()) {
+        range = maxOfArr - minOfArr
+    }
+    return range
 }
 
 
