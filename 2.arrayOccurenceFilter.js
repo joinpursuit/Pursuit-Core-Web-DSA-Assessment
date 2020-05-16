2f5421a40e3e0d0801724808453370536ab13d49
kongnyc <bbsyoow@gmail.com>
2020-05-16 15:11:52 -0400

    start 3

--- 2.arrayOccurenceFilter.js
+++ 2.arrayOccurenceFilter.js
@@ -11,7 +11,24 @@
 // occurrenceFilter([1, 3, 4, 1, 9, 1, 3, 4, 3, 1, 2], 2) returns [1, 3, 4] 
 
 const occurrenceFilter = (arr, n) => {
+    let objArr = {};
+    
+    arr.forEach(el=>{
+        if(!objArr[el]){
+       objArr[el]=1
+       }else{
+         objArr[el]+=1
+       }
+     })
+    
+    let output=[];
+    for(let val in objArr){
+        if(objArr[val]>=n){
+            output.push(Number(val))
+        }
+    }
+    return output
 
 }
 
-// module.exports = occurrenceFilter
+module.exports = occurrenceFilter
