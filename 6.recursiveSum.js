9272704b89c874c0af5c9cd5d0fa6b5dc08d5e23
darsuabasi <uduakabasiabasiurua@pursuit.org>
2020-05-16 17:54:12 -0400

    recurssion

--- 6.recursiveSum.js
+++ 6.recursiveSum.js
@@ -9,14 +9,18 @@
 
 
 const recursiveSum = (arr) => {
-    let i = 0;
     // if my arr has no length, return 0
-    if(i > arr.length) return 0;
-    // add the arr[0] to arr[1] to arr[2] to arr[3] 
-    // to get from arr[0] to arr[1].. ++ 
-    return recursiveSum((arr[i] + 1) + (i + 1))
+    if(i >= arr.length) return 0;
+        // add the arr[0] to arr[1] to arr[2] to arr[3] 
+        // to get from arr[0] to arr[1].. ++ 
+        return recursiveSum(arr, i + 1) + arr[i];
+
+    
+    
 }
 console.log(recursiveSum([5, 2, 9, 11]))
+
+
 
 const iterativeSum = (arr) => {
     let sum = 0; 
@@ -25,13 +29,10 @@ const iterativeSum = (arr) => {
     }
     return sum
 }
-
 console.log(iterativeSum([5, 2, 9, 11]))
 
 
 
 
-
-console.log(recursiveSum([5, 2, 9, 11]))
 
 module.exports = recursiveSum
