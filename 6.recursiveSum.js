548bab5526a56fbd84abb4c7be00a4d9218651f4
ashya <ashyamanning@pursuit.org>
2020-05-16 17:48:46 -0400

    solving problems 5 and 9

--- 6.recursiveSum.js
+++ 6.recursiveSum.js
@@ -1,17 +1,15 @@
 // Q6.
-// Implement a function that accepts an array of integers as an argument. 
-// This function should return the sum of each integer in the array. 
+// Implement a function that accepts an array of integers as an argument.
+// This function should return the sum of each integer in the array.
 // Your solution MUST be **recursive**. Your function must be pure (cannot use global variables)
-// 
+//
 // Example:
 // const arr1 = [5, 2, 9, 11]
 // recursiveSum(arr1) should return 27
 
+const recursiveSum = (arr, i = 0) => {
+  if (i >= arr.length) return 0;
+  return arr[i] + recursiveSum(arr, i + 1);
+};
 
-const recursiveSum = (arr) => {
-
-}
-
-
-
-module.exports = recursiveSum
+module.exports = recursiveSum;
