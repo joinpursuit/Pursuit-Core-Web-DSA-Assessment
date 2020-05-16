b75e1478f589dc8f053c8e401a41beea5bb1a654
Kevin Wong <kevinwong@pursuit.org>
2020-05-16 16:07:20 -0400

    questions 1, 2, 3, 4 complete

--- 2.arrayOccurenceFilter.js
+++ 2.arrayOccurenceFilter.js
@@ -1,17 +1,32 @@
-// Q2. 
+// Q2.
 // Write a function that takes in an array of integers (arr) and a number (n)
-// You function should return an array with only numbers appearing n or more times. 
+// You function should return an array with only numbers appearing n or more times.
 // Your solution MUST work in O(n) time.
 //
 // Example:
 // Input: [1,3,4,1,9,1,3,4,3,1,2], 3
 // Output: [1,3]
-// 
-// occurrenceFilter([1, 3, 4, 1, 9, 1, 3, 4, 3, 1, 2], 3) returns [1, 3] 
-// occurrenceFilter([1, 3, 4, 1, 9, 1, 3, 4, 3, 1, 2], 2) returns [1, 3, 4] 
+//
+// occurrenceFilter([1, 3, 4, 1, 9, 1, 3, 4, 3, 1, 2], 3) returns [1, 3]
+// occurrenceFilter([1, 3, 4, 1, 9, 1, 3, 4, 3, 1, 2], 2) returns [1, 3, 4]
+
+const countOccurences = (arr) => {
+  let counts = {};
+  arr.forEach((el) => {
+    counts[el] ? counts[el]++ : (counts[el] = 1);
+  });
+  return counts;
+};
 
 const occurrenceFilter = (arr, n) => {
+  let output = [];
+  let occurences = countOccurences(arr);
+  for (let key in occurences) {
+    if (occurences[key] >= n) {
+      output.push(Number(key));
+    }
+  }
+  return output;
+};
 
-}
-
-module.exports = occurrenceFilter
+module.exports = occurrenceFilter;
--- 4.queueRange.js
+++ 4.queueRange.js
@@ -1,44 +1,61 @@
 // Q4.
 // Write a function `range` that takes in a Queue of numbers
 // and returns the range (difference between the minimum and maximum).
-// 
+//
 // Example Queue class:
-// 
-// class Queue {
-//   constructor() {
-//     this.array = []
-//   }
-// 
-//   isEmpty() {
-//     return this.array.length === 0
-//   }
-// 
-//   peek() {
-//     if (this.isEmpty()) {
-//       return null
-//     } else {
-//       return this.array[0]
-//     }
-//   }
 //
-//   enqueue(value) {
-//     this.array.push(value)
-//   }
+class Queue {
+  constructor() {
+    this.array = [];
+  }
+
+  isEmpty() {
+    return this.array.length === 0;
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return null;
+    } else {
+      return this.array[0];
+    }
+  }
+
+  enqueue(value) {
+    this.array.push(value);
+  }
+
+  dequeue() {
+    return this.array.shift();
+  }
+}
+
 //
-//   dequeue() {
-//     return this.array.shift()
-//   }
-// }
-// 
 // Imagine the queue contains -145, 45, 11, 98
 // range(q) should return 243
+let q = new Queue();
+q.enqueue(-145);
+q.enqueue(45);
+q.enqueue(11);
+q.enqueue(98);
+
+const findMaxAndMin = (queue) => {
+  if (queue.isEmpty()) return 0;
+  let max = queue.peek();
+  let min = queue.peek();
+  while (!queue.isEmpty()) {
+    max = Math.max(max, queue.peek());
+    min = Math.min(min, queue.peek());
+    queue.dequeue();
+  }
+  return [max, min];
+};
+console.log("max and min: ", findMaxAndMin(q));
 
 // Write your function here
 const range = (queue) => {
+  let res = findMaxAndMin(queue);
+  return res[0] - res[1];
+};
 
-}
-
-
-
-
-module.exports = range
+module.exports = range;
