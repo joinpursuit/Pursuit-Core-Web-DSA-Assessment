19dd21390e0aaeb3eb3c4893ac33a9777de58f27
Dugmar <dugmar.morocho@gmail.com>
2020-05-16 17:16:25 -0400

    test submitions

--- 1.traverseLinkedList.js
+++ 1.traverseLinkedList.js
@@ -1,28 +1,32 @@
 // Q1.
-// Given a linked list, write a function to traverse it 
+// Given a linked list, write a function to traverse it
 // in order, and store each value in an array
 // return an array of values.
-// 
+//
 // example:
-// 
+//
 // class Node {
 //   constructor(val) {
 //     this.next = null
 //     this.val = val
 //   }
 // }
-// 
+//
 // Imagine a linked list with the structure
 // 556 -> 41 -> 33 -> 150 -> 11
-// 
+//
 // traverse(linkedlist) should return:
 // [556, 41, 33, 150, 11]
 
-
 function traverse(list) {
+  let res = [];
+  let current = list;
 
+  while (current) {
+    res.push(current.val);
+    current = current.next;
+  }
+  return res;
 }
 
-
-
-module.exports = traverse
+module.exports = traverse;
