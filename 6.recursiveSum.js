073c3e482d39294778dcf346a764ca25f08e55b2
Kevin Wong <kevinwong@pursuit.org>
2020-05-16 16:24:17 -0400

    num 6 test case success

--- 6.recursiveSum.js
+++ 6.recursiveSum.js
@@ -1,17 +1,16 @@
 // Q6.
-// Implement a function that accepts an array of integers as an argument. 
-// This function should return the sum of each integer in the array. 
+// Implement a function that accepts an array of integers as an argument.
+// This function should return the sum of each integer in the array.
 // Your solution MUST be **recursive**. Your function must be pure (cannot use global variables)
-// 
+//
 // Example:
-// const arr1 = [5, 2, 9, 11]
+const arr1 = [5, 2, 9, 11];
 // recursiveSum(arr1) should return 27
 
+const recursiveSum = (arr, sum = 0) => {
+  if (!arr.length) return sum;
+  return arr.shift() + recursiveSum(arr, sum);
+};
+console.log(recursiveSum(arr1));
 
-const recursiveSum = (arr) => {
-
-}
-
-
-
-module.exports = recursiveSum
+module.exports = recursiveSum;
