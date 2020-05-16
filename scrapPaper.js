7a5ff37546fd8c1f4d031bf4e3cad3a7be2c52b5
maria liza <mariamartinez@pursuit.org>
2020-05-16 14:45:43 -0400

    kindof solve prob 2

--- scrapPaper.js
+++ scrapPaper.js
@@ -1,1 +1,28 @@
 //this is my scrap paper to work out functions
+
+
+// occurrenceFilter([1, 3, 4, 1, 9, 1, 3, 4, 3, 1, 2], 2) 
+// returns [1, 3, 4]
+
+const occurrenceFilter = (arr, n) => {
+  let count = {};
+  arr.forEach((el) => {
+    if (count[el]) {
+      count[el]++;
+    } else {
+      count[el] = 1;
+    }
+  });
+  // return count
+  console.log(count)
+  let output = [];
+  for (let key in count) {
+    if (count[key] === n) {
+        console.log(count[key], "count at key")
+      output.push(parseInt(key));
+      console.log(key, "key")
+    }
+  }
+  return output;
+};
+// console.log(occurrenceFilter([1, 3, 4, 9, 1, 3, 4, 3, 1, 2], 3))
