5c310365b98f452ea3c407db3b287a42f08b7512
darsuabasi <uduakabasiabasiurua@pursuit.org>
2020-05-16 16:33:39 -0400

    logic

--- 2.arrayOccurenceFilter.js
+++ 2.arrayOccurenceFilter.js
@@ -10,15 +10,29 @@
 // occurrenceFilter([1, 3, 4, 1, 9, 1, 3, 4, 3, 1, 2], 3) returns [1, 3] 
 // occurrenceFilter([1, 3, 4, 1, 9, 1, 3, 4, 3, 1, 2], 2) returns [1, 3, 4] 
 
+
 // i want to get the tally of each number 
 // so i want to loop thru the entire arr
 // if a number is there (ex. 3 times) then push that number to the new arr
+// bank / store for count 
 const occurrenceFilter = (arr, n) => {
+    let count = [];
     for(let i = 0; i < arr.length; i++) {
-        
+        let n =
+
     }
+
 
 
 }
 
+
+
+// if(my arr has no length) {
+//     return []
+// } else {
+//     iterate thru the arr and increment count of number in bank if its already there 
+//     then when you fin going thru loop, check bank
+//     whichever num in bank that is >= 3, push to that new arr
+// }
 module.exports = occurrenceFilter
